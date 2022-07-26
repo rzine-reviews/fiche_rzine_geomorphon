d121580ee1b18a1340a9bc2f65da4e0310d7ea08
Paul Passy <paul.passy@u-paris.fr>
2022-07-26 17:30:17 +0200

    Ajout d'éléments de reproductibilité

--- template_readrzine.R
+++
@@ -1,7 +0,0 @@
-library(rzine)
-rmarkdown::draft(file = "index.Rmd", 
-                 template = "readrzine", 
-                 package = "rzine", 
-                 create_dir = FALSE, 
-                 edit = FALSE)
-rmarkdown::render("index.Rmd", envir = new.env())
